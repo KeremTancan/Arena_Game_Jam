d4d4bfc80167b7bb429c5a2090934ddc97e07a94
KeremTancan <tancankerem149@gmail.com>
2024-06-02 12:58:22 +0300

    Added: Score System

---
+++ Assets/Scripts/ScoreCount.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour
+{
+    public Text HScore;
+    public Text Score;
+    private int score;
+    private int hscore;
+
+    void Start()
+    {
+        score = 0;
+        Score.text = score.ToString();
+        
+        hscore = PlayerPrefs.GetInt("HighScore", 0);
+        HScore.text = hscore.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Elmas"))
+        {
+            score++;
+            Score.text = score.ToString();
+            Destroy(other.gameObject);
+            
+            if (score > hscore)
+            {
+                hscore = score;
+                HScore.text = hscore.ToString();
+                
+                PlayerPrefs.SetInt("HighScore", hscore);
+                PlayerPrefs.Save();
+            }
+        }
+    }
+    
+}
