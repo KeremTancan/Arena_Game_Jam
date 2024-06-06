8a1b7b799804f33e459746f4d3e111c43f6b54c3
KeremTancan <tancankerem149@gmail.com>
2024-06-06 11:50:00 +0300

    Added: Particle Effects

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public Transform teleportTarget;
-  
+    public ParticleSystem teleportEffect;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             other.transform.position = teleportTarget.position;
+            
             SoundManager.Instance.PlayEffectSound(SoundManager.Instance.TeleportSound);
+            
+            ParticleSystem effect = Instantiate(teleportEffect, teleportTarget.position, Quaternion.identity);
+            effect.Play();
+            
+            Destroy(effect.gameObject, effect.main.duration + effect.main.startLifetime.constantMax);
         }
     }
 }
